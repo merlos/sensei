4000c422077479f9431b6f351759948575e63dc0
merlos <merlos@gmail.com>
2025-08-04 22:37:55 +0000

    update template to use #defines

--- esp32/sensei/secrets-template.h
+++ esp32/sensei/secrets-template.h
@@ -9,8 +9,8 @@ Update the wifi ssid and password values.
   #define SECRETS_H
 
   // Wifi Setup
-  const char* WIFI_SSID = "ssid";
-  const char* WIFI_PASSWORD = "password";
+  #define WIFI_SSID "ssid"
+  #define WIFI_PASSWORD "password"
 
-  const char* SERVER_API_TOKEN="1234567890";
+  #define SERVER_API_TOKEN "my-secret-token-123"
 #endif
