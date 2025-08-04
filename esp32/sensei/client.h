ba12e236ab029383d34ae7107d7af3ded67fa40f
merlos <merlos@gmail.com>
2025-08-04 22:39:07 +0000

    add the defines in client.h

--- esp32/sensei/client.h
+++ esp32/sensei/client.h
@@ -13,6 +13,12 @@ struct SensorData {
   String value;
 };
 
+// For indicating if the URL is a valid URL
+#define URL_IS_HTTP   1
+#define URL_IS_HTTPS  2
+#define URL_IS_INVALID_HTTP 0
+
+int checkHTTPPrefix(const String& url);
 
 String buildPayload(SensorData sensors[], int size);
 
