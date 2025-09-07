20672f3ee52cf0d9be5c0fa41271bf59098a40d8
merlos <merlos@users.noreply.github.com>
2025-09-07 12:59:11 +0300

    start base ios project

---
+++ ios/Sensei/Sensei/SenseiApp.swift
@@ -0,0 +1,32 @@
+//
+//  SenseiApp.swift
+//  Sensei
+//
+//  Created by Merlos on 9/7/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SenseiApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ios/Sensei/SenseiTests/SenseiTests.swift
@@ -0,0 +1,17 @@
+//
+//  SenseiTests.swift
+//  SenseiTests
+//
+//  Created by Merlos on 9/7/25.
+//
+
+import Testing
+@testable import Sensei
+
+struct SenseiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
